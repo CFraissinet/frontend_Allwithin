b8192e06165d42ed60fdc4942cb2cc365ba39145
syu99 <ufcmjohan@gmail.com>
2023-05-15 11:40:35 +0200

    implementation integration of the homePage with the navbar the buttons and the bi-color background

--- components/Home.js
+++ components/Home.js
@@ -2,14 +2,25 @@ import styles from '../styles/Home.module.css';
 
 function Home() {
   return (
-    <div>
-      <main className={styles.main}>
+    <div className={styles.homeContainer}>
+    <div className={styles.backgroundTop}>
+        <div className={styles.navbar}>
+            <div className={styles.logoContainer}>
+                <img className={styles.logo} src="/path/to/your/logo.png" alt="Logo" />
+            </div>
+            <div className={styles.buttons}>
+                <button className={styles.button}>Sign Up</button>
+                <button className={styles.button}>Sign In</button>
+            </div>
+        </div>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+            Welcome to <span className={styles.highlight}>AllWithin</span>
         </h1>
-      </main>
+        <h2 className={styles.subtitle}>Where projects come to life</h2>
     </div>
-  );
+    <div className={styles.backgroundBottom}></div>
+</div>
+);
 }
 
 export default Home;
