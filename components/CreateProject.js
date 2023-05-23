499c5fb7b10e727f783c7f253ca41fb09fb1af6e
syu99 <ufcmjohan@gmail.com>
2023-05-23 11:15:26 +0200

    creation of the component jobBox allowing to select a job in the scroll menu and addition of the scroll navigation + optimization of the responsive

--- components/CreateProject.js
+++ components/CreateProject.js
@@ -2,26 +2,37 @@ import Link from "next/link";
 import styles from "../styles/CreateProject.module.css";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faUser } from "@fortawesome/free-solid-svg-icons";
-import { useState } from 'react';
-import { useRouter } from 'next/router';
+import { useState } from "react";
+import { useRouter } from "next/router";
+import Select from 'react-select';// library to add the drop down menu with checkboxes
+
+
+
 
 function CreateProject() {
+
   const router = useRouter();
+
+
+  
 
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
   const [start_date, setStart_date] = useState("");
   const [end_date, setEnd_date] = useState("");
   // const [token, setToken] = useState(""); // ADDED: State for storing the user's token
+  const [jobBox, setJobBox] = useState([<JobCard isFirst={true} />]);
 
-  const [counter, setCount] = useState(0); // initialization hook
+
+  
+  
 
   const clickCreatProject = () => {
     const data = {
       name: name,
       description: description,
-      start_date: null,
-      end_date: null,
+      start_date: start_date,
+      end_date: end_date,
       // token: token, // ADDED: Including the user's token in the request body
     };
 
@@ -30,29 +41,42 @@ function CreateProject() {
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify(data),
     })
-    .then((response) => response.json())
-    .then((data) => {
-      console.log(data)
-    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    };
 
-          // dispatch(login({ token: data.token })); Uncomment this line after defining dispatch and login
 
-        
-      
+// const jobCard= (   <div className={styles.jobInputContainer}>
+//   <div className={`${styles.inputBox} ${styles.labelStyle}`}>
+//     <label htmlFor="jobProfile">Job Profile:</label>
+//     <input
+//       type="text"
+//       id="jobProfile"
+//       className={styles.jobProfile}
+//       placeholder="Enter job profile"
+//     />
+//   </div>
+//   <div className={styles.counter}>
+//     <div>
+//       <button onClick={() => decrement()}>-</button>
+//       <span> {counter} </span>
+//       <button onClick={() => increment()}>+</button>
+//     </div>
+//     <button>X</button>
+//   </div>
+// </div>)
 
   
-  
-  const increment = () => {
-    setCount(counter + 1);  // setter parameterization that increments counter
-  }
-  
-  const decrement = () => {
-    if (counter > 0) {
-      setCount(counter - 1);   // setter parameterization that decrements the counter and prevents the counter from going below 0
-    }
-  }
 
-  }
+
+const addMemberClick = () => {
+setJobBox([...jobBox, <JobCard isFirst={false}/>])
+  console.log("click1111111111111111");	
+
+};
+
   return (
     <div className={styles.background}>
       <div className={styles.homeContainer}>
@@ -78,97 +102,150 @@ function CreateProject() {
         </div>
         {/* navbar end*/}
 
-{/* contien la div formContainer et txtAreaContainer */}
+        {/* contien la div formContainer et txtAreaContainer */}
         <div className={styles.leftRightContainer}>
           <h1>Create Your Project</h1>
           <div className={styles.leftRight}>
             <div className={styles.formContainer}>
               {/*--------------------------- Nav ------------------------------*/}
 
-
-          {/*--------------------------- Forms ------------------------------*/}
-          <div className={styles.inputDiv}>
-              <div className={`${styles.inputBox} ${styles.labelStyle}`}>
-              <label htmlFor="projectName">Project Name:</label>
-              <input
-                type="text"
-                id="projectName"
-                className={styles.input}
-                placeholder="Enter project name"
-              />
-            </div>
-            {/*input  Start Date & End Date FORMS */}
-            <div className={styles.endSartContainer}>
-            <div className={`${styles.inputBox} ${styles.labelStyle}`}>
-                <label htmlFor="startDate">Start Date:</label>
-                <input
-                  type="text"
-                  id="startDate"
-                  className={styles.endStart}
-                  placeholder="Enter start date"
-                />
-              </div>
-
-              <div className={`${styles.inputBox} ${styles.labelStyle}`}>
-                <label htmlFor="endDate">End Date:</label>
-                <input
-                  type="text"
-                  id="endDate"
-                  className={styles.endStart}
-                  placeholder="Enter end date"
-                />
-              </div>
-            </div>
-            {/* input Start Date & End Date FORMS */}
-
-                {/* input job Profile & member count */}
-
-            <div className={styles.jobInputContainer}>
-             
-             
-            <div className={`${styles.inputBox} ${styles.labelStyle}`}>
-                <label htmlFor="jobProfile">Job Profile:</label>
-                <input
-                  type="text"
-                  id="jobProfile"
-                  className={styles.jobProfile}
-                  placeholder="Enter job profile"
-                />
-              </div>      
-                <div className={styles.counter}>
-                  <div>
-                    <button>-</button>
-                    <span>0</span>
-                    <button>+</button>
-                  </div>
-                  <button>X</button>
+              {/*--------------------------- Forms ------------------------------*/}
+              <div className={styles.inputDiv}>
+                <div className={`${styles.inputBox} ${styles.labelStyle}`}>
+                  <label htmlFor="projectName">Project Name:</label>
+                  <input
+                    type="text"
+                    id="projectName"
+                    onChange={(e) => setName(e.target.value)}
+                     value={name}
+                    className={styles.input}
+                    placeholder="Enter project name"
+                  />
                 </div>
-            </div>
-            <button className={styles.addButton}>Add Member</button>
-            {/* input job Profile & member count */}
-          </div>
-        </div>
-          {/*--------------------------- Forms ------------------------------*/}
-    
+                {/*input  Start Date & End Date FORMS */}
+                <div className={styles.endSartContainer}>
+                  <div className={`${styles.inputBox} ${styles.labelStyle}`}>
+                    <label htmlFor="startDate">Start Date:</label>
+                    <input
+                      type="date"
+                      id="startDate"
+                      className={styles.endStart}
+                      placeholder="Enter start date"
+                    />
+                  </div>
 
-        {/*------------------------- div txtAreaContainer ----------------------*/}
-        <div  className={styles.txtAreaContainer}>
-        <label className={styles.labelStyle} htmlFor="projectDescription">Project description:</label>
-    <textarea 
-    maxLength="1000"
-    className={styles.txtArea} 
-    placeholder="Enter project description"
-    ></textarea>
-    <div className={styles.btnCreatCountainer}>
-    <button className={styles.btnCreateProject}> CREATE PROJECT</button>
-    </div>
-        </div>
-        </div>
-        {/*------------------------- div txtAreaContainer fin ----------------------*/}
+                  <div className={`${styles.inputBox} ${styles.labelStyle}`}>
+                    <label htmlFor="endDate">End Date:</label>
+                    <input
+                      type="date"
+                      id="endDate"
+                      className={styles.endStart}
+                      placeholder="Enter end date"
+                    />
+
+                  </div>
+
+
+                </div>
+                {/* input Start Date & End Date FORMS */}
+
+
+
+
+                
+                {/* input job Profile & member count */}
+              <div className={styles.jobInputContainer}>
+               {jobBox}
+              </div>
+              </div>
+                <button onClick={() => addMemberClick()}  className={styles.addButton}>Add Member</button>
+
+
+            </div>
+            {/*--------------------------- Forms ------------------------------*/}
+
+            {/*------------------------- div txtAreaContainer ----------------------*/}
+            <div className={styles.txtAreaContainer}>
+                         <label className={styles.labelStyle} htmlFor="projectDescription">
+                Project description:
+              </label>
+              <textarea
+              onChange={(e) => setDescription(e.target.value)}
+              value={description}
+                maxLength="1000"
+                className={styles.txtArea}
+                placeholder="Enter project description"
+              ></textarea>
+              <div className={styles.btnCreatCountainer}>
+                <button onClick={() => clickCreatProject()}  className={styles.btnCreateProject}>
+                
+                  CREATE PROJECT
+                </button>
+              </div>
+            </div>
+          </div>
+          {/*------------------------- div txtAreaContainer fin ----------------------*/}
         </div>
         {/* contains the div formContainer and txtAreaContainer */}
       </div>
     </div>
   );
 }
+
+
+function JobCard(props) {
+ 
+  
+  const [counter, setCounter] = useState(0); // initialization hook
+  const [selectedOptions, setSelectedOptions] = useState([]); // initialization hook for the drop down menu with checkboxes
+
+  const options = [
+    { value: 'frontend', label: 'Développeur Frontend' },
+    { value: 'backend', label: 'Développeur Backend' },
+  ];
+  const increment = () => {
+    setCounter(counter + 1); // setting of the switch that increments the counter of the jobCard drop-down menu
+  };
+  
+  const decrement = () => {
+    if (counter > 0) {
+      setCounter(counter - 1); // setter parameterization that decrements the counter and prevents the counter from going below 0
+    }
+  };
+  
+  const handleSelectChange = (selected) => {
+    setSelectedOptions(selected);
+  };
+  
+return (
+  <div>
+  <div className={`${styles.inputBox} ${styles.labelStyle}`}>
+    <label htmlFor="jobProfile">Job Profile:</label>
+    <Select
+      id="jobProfile"
+      className={styles.jobProfile}
+      options={options}
+      isMulti
+      value={selectedOptions}
+      onChange={handleSelectChange}
+    />
+  </div>
+  <div className={styles.counter}>
+    <div>
+      <button onClick={() => decrement()}>-</button>
+      <span> {counter} </span>
+      <button onClick={() => increment()}>+</button>
+    </div>
+    {
+      !props.isFirst && <button >X</button>
+    }
+  </div>
+  </div>
+
+);
+}
+
+
+
 export default CreateProject;
+
