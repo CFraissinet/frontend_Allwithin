826a960ecac11d56c40cc445037c687d9156c5ba
Benoit <giannini.benoit@gmail.comwd>
2023-05-23 21:58:33 +0200

    testing

--- components/Dashboard.js
+++ components/Dashboard.js
@@ -1,14 +1,51 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import styles from "../styles/Dashboard.module.css";
-import Link from "next/link";
 import { useDispatch, useSelector } from "react-redux";
 import { removeProject } from "../reducers/project";
+import Modal from "react-modal";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faXmark,
+  faEnvelope,
+  faPhone,
+} from "@fortawesome/free-solid-svg-icons";
 
 function Dashboard() {
   const project = useSelector((state) => state.project.value);
-  console.log(project);
+  // console.log(project);
   const [btnProject, setBtnProject] = useState(false);
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [jobTitle, setJobTitle] = useState("");
+  const [projectData, setProjectData] = useState([]);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const idProject = project[0]._id;
+    console.log(idProject);
+    fetch(`http://localhost:3000/offers/${idProject}`)
+      .then((response) => response.json())
+      .then((data) => {
+        // console.log(data.projectData);
+        setProjectData(data.projectData);
+        console.log(data.projectData);
+        // if (dataProjects.length !== 0) {
+        //   setSelectProject(data.projects[0]);
+        // } else {
+        //   setSelectProject({ name: "", description: "" });
+        // }
+      });
+  }, []);
+
+  console.log(projectData);
+
+  function openModal(title) {
+    setJobTitle(title);
+    setIsOpen(true);
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
 
   const toolsBtn = (
     <>
@@ -23,6 +60,7 @@ function Dashboard() {
   //For clear store
   const clearProject = (project) => {
     dispatch(removeProject(project));
+    setProjectData([]);
     location.href = "/lobby";
   };
 
@@ -31,9 +69,12 @@ function Dashboard() {
       <div className={styles.navBar}>
         {/* NAV BAR */}
         <div className={styles.btnContainer}>
-          <Link href="/lobby">
-            <button className={styles.btnNavBar}>My lobby</button>
-          </Link>
+          <button
+            onClick={() => clearProject(project)}
+            className={styles.btnNavBar}
+          >
+            My lobby
+          </button>
           <button className={styles.btnNavBar}>My message</button>
         </div>
       </div>
@@ -49,11 +90,15 @@ function Dashboard() {
               <div className={styles.midCard}>
                 <div className={styles.containerPost}>
                   <b className={styles.postPending}>Post Pending :</b>
-                  <div className={styles.post}> POST 1</div>
-                  <div className={styles.post}> POST 2</div>
-                  <div className={styles.post}> POST 3</div>
-                  <div className={styles.post}> POST 4</div>
-                  <div className={styles.post}> POST 5</div>
+                  {projectData.map((data, i) => (
+                    <div
+                      onClick={() => openModal(data.name)}
+                      className={styles.post}
+                      key={i}
+                    >
+                      {data.name}
+                    </div>
+                  ))}
                 </div>
               </div>
               <div className={styles.midCard}>
@@ -67,12 +112,6 @@ function Dashboard() {
             </div>
             <div className={styles.containerStartBtn}>
               <button
-                onClick={() => clearProject(project)}
-                className={styles.btnStart}
-              >
-                Back lobby
-              </button>
-              <button
                 onClick={() => setBtnProject(true)}
                 className={styles.btnStart}
               >
@@ -82,6 +121,58 @@ function Dashboard() {
           </div>
         </div>
       </div>
+      <Modal
+        ariaHideApp={false}
+        isOpen={modalIsOpen}
+        onRequestClose={closeModal}
+        className={styles.modalOpen}
+      >
+        <div className={styles.container}>
+          <FontAwesomeIcon
+            className={styles.btnClose}
+            onClick={closeModal}
+            icon={faXmark}
+          />
+          <b className={styles.b}>{jobTitle}</b>
+          <div className={styles.containerCard}>
+            {projectData.map((data, i) => (
+              <div className={styles.card} key={i}>
+                <div className={styles.cardContainer}>
+                  <img className={styles.photo} src={data.user_Id[0].photo} />
+                  <div className={styles.containerContact}>
+                    <b className={styles.name}>
+                      {data.user_Id[0].firstname} {data.user_Id[0].name}
+                    </b>
+                    <div className={styles.gitHub}>
+                      <img
+                        className={styles.contactGitHub}
+                        src="/images/github-mark.svg"
+                        alt="GitHub"
+                      />
+                      <b>TheRock</b>
+                    </div>
+                    <div className={styles.mail}>
+                      <FontAwesomeIcon
+                        className={styles.contact}
+                        color="black"
+                        icon={faEnvelope}
+                        alt="E-Mail"
+                      />
+                      <a href="mailto:{data.user_Id[0].email}">
+                        {data.user_Id[0].email}
+                      </a>
+                    </div>
+                    <div className={styles.tel}>
+                      <FontAwesomeIcon icon={faPhone} />
+                      <a href="tel:0609080706">06.09.08.07.06</a>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      </Modal>
     </div>
   );
 }
